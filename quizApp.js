5ed0e24c7818383e5218ec6a33052f68a567292b
Audrey Foss <porcupine4@gmail.com>
2019-04-04 21:29:29 -0500

    initial commit

---
+++ quizApp.js
@@ -0,0 +1,44 @@
+// A function that listens for button clicks and submits its ID to the decideView()
+
+// A function that on form submit checks for if the answer is correct
+    // then submits the view and any extra IDs to the decideView()
+
+// A function that decides which view to change to
+    // 1st arg = current view
+
+// A function that checks the view num and updates the button text
+    // Make an object with the possible texts
+
+// A function that renders
+
+// <!-- View 1 for starting Page -->
+
+// // <div class="view0">
+// //     <div id='welcomePicture'>
+// //         <img src="https://longdonclub.co.uk/wp/wp-content/uploads/2017/12/Quiz@Kvarteret-e1518802228786.png"
+// //             alt='Unable to Load Image'></img>
+// //     </div>
+// {/* <button id="quizBtn">Start Quiz</button> */}
+// // </div>
+        
+// // /* <!-- View 2 for Question Page --> */
+// //         <div class="view2" role="QuestionPage">
+// //             <div class="questionPanel">
+
+// //             </div>
+// //             <div class="answerPanel">
+
+// //             </div>
+// {/* <button id="quizBtn">Start Quiz</button> */}
+// //         </div>
+
+// // /* <!-- View 3 for Response Page --> */
+// //         <div class="view3" role="ResponsePage">
+// //             <div class="responsePanel">
+
+// //             </div>
+// //             <div class="correctAnsPanel">
+
+// //             </div>
+// {/* <button id="quizBtn">Start Quiz</button> */}
+// //         </div>
